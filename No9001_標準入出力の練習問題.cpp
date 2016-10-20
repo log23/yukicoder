fea42fc2201a3473998f6bffcacf86c383941e83
log23 <11215hiro@gmail.com>
2016-10-20 13:52:32 +0900

    No.9001 標準入出力の練習問題（テスト用）
    問題文
    
    整数AA,BBと文字列SSが与えられるので、
    
    A+BA+Bの結果と SSの文字列を半角空白区切りで出力してください。
    値の受け取り方・出力がわからない方は 言語別標準入出力も参考にしてください。 
    入力
    
    A B
    S
    AAとBBは整数
    1≤A≤10001≤A≤1000 
    1≤B≤10001≤B≤1000
    SSは 長さ10以下の半角英数の文字列
    
    出力
    
    N S
    NNはA+BA+Bの結果です。
    SSは入力の文字列そのまま出力してください。

---
+++ No9001_標準入出力の練習問題.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	int a = 0;
+	int b =0;
+	string s;
+	
+	cin >> a >> b;
+	cin >> s;
+	
+	cout << a + b << " " << s << endl;
+	
+	return 0;
+}
